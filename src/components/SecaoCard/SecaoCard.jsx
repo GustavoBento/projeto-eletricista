507ed1ef17ee5bd63fab5680a3074c7963715777
GustavoBento <gustavo_grb-@hotmail.com>
2024-04-19 19:49:52 -0300

    finalizando seção card

--- src/components/SecaoCard/SecaoCard.jsx
+++ src/components/SecaoCard/SecaoCard.jsx
@@ -11,7 +11,7 @@ export default function SecaoCard() {
     return (
         <section className='secao-card'>
             <h2>Meus serviços de eletricista em salvador</h2>
-            <div className='secao-card-lista1'>
+            <div className='secao-card-lista'>
                 <Card 
                     imagem={icone1}
                     destaque="Eletricista"
@@ -20,12 +20,32 @@ export default function SecaoCard() {
                 />
                 <Card 
                     imagem={icone2}
+                    destaque={"Eletricista"}
+                    titulo={"Comercial"}
+                    texto={"Instalação de qualidade para que o seu negócio receba a carga necessárias para alimentar os equipamentos elétricos e iluminação local, sem cargas desnecessárias."}
+                />
+                <Card 
+                    imagem={icone3}
+                    destaque={"Manutenção"}
+                    titulo={"Elétrica"}
+                    texto={"Manutenção elétrica e correção de cargas em Salvador, incluindo novas ligações, medidor, projetos de entradas, análise de carga instalada, instalação de tomadas, etc."}
+                />
+            </div>
+            <div className='secao-card-lista'>
+                <Card 
+                    imagem={icone4}
+                    destaque={"Projeto"}
+                    titulo={"Elétrico"}
+                    texto={"Atuo constantemente na elaboração de projetos elétricos em Salvador, seja ele em infraestruturas ou prestação de consultorias técnicas."}
+                />
+                <Card 
+                    imagem={icone5}
                     destaque={"Chuveiro"}
                     titulo={"Elétrico"}
                     texto={"Por mais que seja apenas um chuveiro, é necessário ter cuidado e cautela para realizar a instalação, portanto, contrate um profissional."}
                 />
                 <Card 
-                    imagem={icone3}
+                    imagem={icone6}
                     destaque={"Padrão"}
                     titulo={"Coelba"}
                     texto={"Se você busca por um Eletricista em Salvador que trabalha com Padrão e Normas Exigidas pela Coelba, basta entrar em contato."}
