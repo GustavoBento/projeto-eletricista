9aa9500c242d4b13036410491d53956ac12f1084
GustavoBento <gustavo_grb-@hotmail.com>
2024-04-22 20:24:16 -0300

    finalizando seção maps
    finalizei a seção maps com responsividade. Usei um iframe do google maps.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Banner from "./components/Banner/Banner";
 import Menu from "./components/Menu/Menu";
 import SecaoCard from "./components/SecaoCard/SecaoCard";
+import SecaoMaps from "./components/SecaoMaps/SecaoMaps";
 import SecaoResponsabilidade from "./components/SecaoResponsabilidade/SecaoResponsabilidade";
 import SecaoRisco from "./components/SecaoRisco/SecaoRisco";
 import SecaoServico from "./components/SecaoServico/SecaoServico";
@@ -15,6 +16,7 @@ export default function App() {
       <SecaoResponsabilidade />
       <SecaoCard />
       <SecaoServico />
+      <SecaoMaps />
     </div>
   )
 }
---
+++ src/components/SecaoMaps/SecaoMaps.jsx
@@ -0,0 +1,21 @@
+import "./SecaoMaps.css";
+
+export default function SecaoMaps() {
+  return (
+    <section className="secao-maps">
+      <h3>
+        minha <span>localização</span>{" "}
+      </h3>
+      <iframe
+        title="mapa do eletricista"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d178269.778243947!2d-43.40681792686388!3d-22.897015751831148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x997d03b5e41409%3A0x316f7c1160fbe18c!2sBeto%20Eletricista!5e0!3m2!1spt-BR!2sbr!4v1713827170335!5m2!1spt-BR!2sbr"
+        width="100%"
+        height="500"
+        style={{border: 0}}
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </section>
+  );
+}
